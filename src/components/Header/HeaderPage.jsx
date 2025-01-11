e99b6235728569c796c6313bd316c9f701fee036
Akmalbekov070 <akmalbekov2022@gmail.com>
2025-01-11 21:55:59 +0300

    uplload clerk commit

--- src/components/Header/HeaderPage.jsx
+++ src/components/Header/HeaderPage.jsx
@@ -5,24 +5,34 @@ import React from 'react';
 
 export default function HeaderPage() {
 	return (
-		<Box w={'full'} h={24} bg={'blue.600'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} px={20}>
+		<Box w={'full'} h={24} bg={'blue.700'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} px={20}>
 			{/* left header  */}
-			<HStack>
-				<Image
-					w={24}
-					src='https://i.postimg.cc/SxydV0n1/removal-ai-e2c59107-02d4-4e7b-a4bc-dd5d9d16f024-photo-2024-11-04-10-22-39.png'
-					alt='Image Genius'
-				/>
-				<Text w={'100px'} fontStyle={'-moz-initial'} fontFamily={'mono'} fontSize={'xl'}>
-					Genius Academy
-				</Text>
-			</HStack>
+			<Link href={'/'}>
+				<HStack>
+					<Image
+						w={24}
+						src='https://i.postimg.cc/SxydV0n1/removal-ai-e2c59107-02d4-4e7b-a4bc-dd5d9d16f024-photo-2024-11-04-10-22-39.png'
+						alt='Image Genius'
+					/>
+					<Text w={'100px'} fontStyle={'-moz-initial'} fontFamily={'mono'} fontSize={'xl'}>
+						Genius Academy
+					</Text>
+				</HStack>
+			</Link>
 			{/* right header */}
-			<HStack gap={16} fontSize={'md'}>
-				<Link href='/'>About Genius</Link>
-				<Link href='/'>Teachers</Link>
-				<Link href='/'>Students</Link>
-				<Link href='/'>Results</Link>
+			<HStack gap={16} fontSize={'md'} fontFamily={'cursive'}>
+				<Link href='/' className='hover:text-yellow-500'>
+					About Genius
+				</Link>
+				<Link href='/' className='hover:text-yellow-500'>
+					Teachers
+				</Link>
+				<Link href='/' className='hover:text-yellow-500'>
+					Students
+				</Link>
+				<Link href='/' className='hover:text-yellow-500'>
+					Results
+				</Link>
 				{/* clerk this */}
 			</HStack>
 		</Box>
